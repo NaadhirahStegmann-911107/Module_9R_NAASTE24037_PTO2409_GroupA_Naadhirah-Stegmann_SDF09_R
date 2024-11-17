86ee8a8c9a52bda2a8be432cb251b29352b2681c
NaadhirahStegmann-911107 <naadhirahsteg@gmail.com>
2024-11-18 00:00:17 +0200

    Edit and new instruction
    Created the folder for the background image. Created the new instruction that noted the message to User.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let hasBlackJack = false
 let isAlive = true 
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
 function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -20,7 +22,6 @@ function startGame() {
         message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)
 }
  
 
